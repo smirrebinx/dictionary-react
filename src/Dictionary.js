c3a37aa0a84e11b979cb1e92af5b515254fd8be3
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-09 17:04:22 +0200

    Add definitions and show search results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 /*react-dictionary-app*/
 import React, { useState } from "react";
 import axios from "axios";
+import SearchResults from "./SearchResults";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   //Documentation: https://dictionaryapi.dev/
@@ -42,6 +44,7 @@ export default function Dictionary() {
           </div>
         </div>
       </form>
+      <SearchResults results={results} />
     </div>
   );
 }
