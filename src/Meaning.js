7c85aa47b5866fff769c0c1cdcb9b8def73722f4
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-11 15:25:16 +0200

    Change image

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,30 @@
 import React from "react";
+import bookPile from "./images/bookPile.jpg";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div>
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <div className="card">
+        <div className="card-thumb">
+          <img
+            src={bookPile}
+            className="img-fluid d-block card-img"
+            alt="Words in a pile"
+          />
+          <h3 className="word-class">{props.meaning.partOfSpeech}</h3>
+        </div>
+      </div>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
+            <div>
+              <p>{definition.definition}</p>
               <br />
-              <em>{definition.example}</em>
-            </p>
+              <p>
+                <em>{definition.example}</em>
+              </p>
+            </div>
           </div>
         );
       })}
