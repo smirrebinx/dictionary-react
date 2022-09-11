c5c2ee6c94b64923871c9e5f7275ddca231a77ae
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-11 13:44:00 +0200

    Add images to folder

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -5,7 +5,7 @@ export default function SearchResults(props) {
   if (props.results) {
     return (
       <div>
-        <h2>{props.results.word}</h2>
+        <h2 className="searched-word">{props.results.word}</h2>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
