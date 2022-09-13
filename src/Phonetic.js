bf210c7e58b278abaa813e74c5a65c834e3da927
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-13 12:13:44 +0200

    Add phonetic text

---
+++ src/Phonetic.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div>
+      <span>{props.phonetic.text}</span>
+    </div>
+  );
+}
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,16 +1,26 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function SearchResults(props) {
   if (props.results) {
     return (
       <div>
-        <h2 className="searched-word">{props.results.word}</h2>
+        <section>
+          <h2 className="searched-word">{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meaning meaning={meaning} />
-            </div>
+            </section>
           );
         })}
       </div>
