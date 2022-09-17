fb561a65ae426ae0f134add6691774ee3ddbeaaf
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-17 14:52:21 +0200

    Change amount of images from API request

--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
@@ -7,7 +8,7 @@ export default function Photos(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-3" key={index}>
+              <div className="col-4" key={index}>
                 <img
                   src={photo.src.landscape}
                   className="img-fluid mb-4 rounded"
