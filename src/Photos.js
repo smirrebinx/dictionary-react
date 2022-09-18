f16ff904127c955dc24996864eeb0f454e7c2e92
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-18 14:59:04 +0200

    Add alt to images

--- src/Photos.js
+++ src/Photos.js
@@ -17,7 +17,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid mb-4 searched-word-images"
-                    alt={photo.src.photographer}
+                    alt={photo.alt}
                   />
                 </a>
               </div>
