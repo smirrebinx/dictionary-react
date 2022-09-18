651be8ab39eaa2ca5c8f09b5dc2898d8b20c215c
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-18 13:51:04 +0200

    Change styling audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,14 +5,8 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="phonetic">
-      <div className="row">
-        <div className="col-2">
-          <ReactAudioPlayer src={props.phonetic.audio} controls />
-        </div>
-        <div className="col-2">
-          <span className="phonetic-text">{props.phonetic.text}</span>
-        </div>
-      </div>
+      <ReactAudioPlayer src={props.phonetic.audio} controls />
+      <span className="phonetic-text">{props.phonetic.text}</span>
     </div>
   );
 }
