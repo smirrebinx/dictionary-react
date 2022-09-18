31caef6c13a0cbf97ac9e4f28fa79551853e9938
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-18 11:13:46 +0200

    Add styling to inages

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ export default function Photos(props) {
               <div className="col-4" key={index}>
                 <img
                   src={photo.src.landscape}
-                  className="img-fluid mb-4 rounded"
+                  className="img-fluid mb-4 searched-word-images"
                   alt={photo.src.photographer}
                 />
               </div>
