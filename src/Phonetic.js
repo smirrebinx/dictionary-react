89f3401f9f985e6b7604dd60656f681f6b4f311c
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-18 13:24:52 +0200

    Add audio to npm react audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./Phonetic.css";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   return (
     <div className="phonetic">
       <div className="row">
         <div className="col-2">
-          <a
-            href={props.phonetic.audio}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Listen
-          </a>
+          <ReactAudioPlayer src={props.phonetic.audio} controls />
         </div>
         <div className="col-2">
           <span className="phonetic-text">{props.phonetic.text}</span>
